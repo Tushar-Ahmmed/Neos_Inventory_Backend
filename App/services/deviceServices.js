7f3887c4d2fd355b933b0a4c3f8f70fe084aa4d5
Tushar Ahmmed <tushar.ahmmed@neoscoder.com>
2025-02-28 15:05:08 +0600

    last stage in my desktop. Before pull on 28.02.25

--- App/services/deviceServices.js
+++ App/services/deviceServices.js
@@ -35,30 +35,30 @@ export const assignDeviceService = async (req) => {
       const ObjectId = new mongoose.Types.ObjectId("67af3b12a55a523a84f93b73")
       if(computer.Assigned_To.equals(ObjectId)) {
          let user = await UsersModel.findOne({ Email: email })
-      if(!user) {
-         return { "status":"Error", message: "User not found" }
-      }
+         if(!user) {
+            return { "status":"Error", message: "User not found" }
+         }
 
-      await ComputersModel.updateOne({ Serial: serial }, { $set: { Assigned_To: user._id } })
-    //   assign to shesh now have to create an object that contains computer serial number and assign date and a null return date.
-    if(assign_date === undefined || assign_date === null || assign_date === "")  {
-       assign_date = new Date()
-      }
-      let assign = { Device_id: computer._id, AssignDate: assign_date, UnAssignDate: null }
+         await ComputersModel.updateOne({ Serial: serial }, { $set: { Assigned_To: user._id } })
+
+         if(assign_date === undefined || assign_date === null || assign_date === "")  {
+            assign_date = new Date()
+         }
+         let assign = { Device_id: computer._id, AssignDate: assign_date, UnAssignDate: null }
          let result = await UsersModel.findOneAndUpdate(
             { Email: email },
             { $push: { Devices: assign } },
             { new: true }
-        );
+         );
 
         if(!result) {
             return { "status":"Error", message: "Device not assigned" }
         }
         return { "status":"Success", message: result }
-        }
-        else{
+      }
+      else{
          return { "status":"Error", message: "Device already assigned" }
-        }
+      }
 
    } catch (error) {      
       return { "status":"Error", message: error.message }
