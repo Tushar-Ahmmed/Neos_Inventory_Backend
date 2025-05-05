b650e34b5d6fbe2953ac30cc9f407a06707640a8
Tushar Ahmmed <tushar.ahmmed@neoscoder.com>
2025-05-05 23:51:29 +0600

    some light changes are made in the user's laptop.

--- App/services/categoryServices.js
+++ App/services/categoryServices.js
@@ -3,20 +3,20 @@ import CategoryModel from '../models/AccessoriesCategoriesModel.js'
 
 export const createCategoryService = async(req)=>{
     
+    try {
         const Title = req.body
-        try {
-            await CategoryModel.create(Title)
-            return { "status":"Success", message: "Category added successfully" }
-        } catch (error) {
-            return { "status":"Error", message: error.message }
-        }
+        await CategoryModel.create(Title)
+        return { "status":"Success", message: "Category added successfully" }
+    } catch (error) {
+        return { "status":"Error", message: error.message }
+    }
     
 }
 
 export const updateCategoryService = async(req)=>{
-    const catID = req.params.id
-    const Title = req.body
     try {
+        const catID = req.params.id
+        const Title = req.body
         let result =await CategoryModel.findByIdAndUpdate(catID, Title)
         if(!result){
             return { "status":"Error", message: "Category not found" }
@@ -28,8 +28,8 @@ export const updateCategoryService = async(req)=>{
 }
 
 export const deleteCategoryService = async(req)=>{
-    const catID = req.params.id
     try {
+        const catID = req.params.id
         let result =await CategoryModel.findByIdAndDelete(catID)
         if(!result){
             return { "status":"Error", message: "Category not found" }
